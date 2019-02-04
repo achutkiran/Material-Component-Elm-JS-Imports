d89cc7bc066b18cc383a795c99fca5be928a113a
Achut Cherukuri <Achut.Cherukuri@primeroedge.com>
2019-02-04 11:13:53 -0600

    Initial commit

---
+++ material.js
@@ -0,0 +1,23 @@
+import './material.css'
+import 'regenerator-runtime/runtime'
+
+// WebComponents
+// import '@webcomponents/webcomponentsjs/webcomponents-loader'
+import '@webcomponents/webcomponentsjs/webcomponents-bundle'
+import '@webcomponents/webcomponentsjs/custom-elements-es5-adapter'
+
+// Material Web Components
+import '@material/mwc-button/mwc-button'
+import '@material/mwc-switch/mwc-switch'
+import '@material/mwc-checkbox/mwc-checkbox'
+import '@material/mwc-icon/mwc-icon'
+// Paper (Polymer) Components
+import '@polymer/paper-input/paper-input'
+import '@polymer/iron-icon/iron-icon'
+import '@polymer/iron-icons/iron-icons'
+import '@polymer/paper-icon-button/paper-icon-button'
+import '@polymer/paper-tabs/paper-tabs'
+import '@polymer/paper-tabs/paper-tab'
+import '@polymer/paper-progress/paper-progress'
+import '@polymer/paper-spinner/paper-spinner-lite'
+import '@polymer/paper-fab/paper-fab'
